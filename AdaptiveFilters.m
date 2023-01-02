aa6b6b9551311c0c29d6bdeaf257191b0cd9a95a
shuvipas <110031174+shuvipas@users.noreply.github.com>
2023-01-02 16:05:39 +0200

    Update AdaptiveFilters.m

--- AdaptiveFilters.m
+++ AdaptiveFilters.m
@@ -3,8 +3,8 @@ n =10*Fs;
 alfa = 0.5;
 G = randn(n, 1);
 b = 1;
-a = [1, -alfa];
-X = filter(b, a, G);
+a1 = [1, -alfa];
+X = filter(b, a1, G);
 N = randn(n, 1);
 Z = X + N;
 
@@ -12,7 +12,7 @@ empMeanZ = 1/n*sum(Z);
 empVarZ = 1/n*sum(Z.^2);
 
 scaledZ = sqrt(3/14).*Z;
-sound(scaledZ, Fs);
+%sound(scaledZ, Fs);
 
 alfa2 = 0.9;
 N2 = randn(n, 1);
@@ -44,13 +44,11 @@ end
 %R3 = toeplitz(RVec(3, 1:3));
 %R4 = toeplitz(RVec(4, 1:4));
 %R5 = toeplitz(RVec(5, :));
-
 %p1 = p(1,1);
 %p2 = p(2,1:2);
 %p3 = p(3,1:3);
 %p4 = p(4,1:4);
 %p5 = p(5,1:5);
-
 %W1 = (inv(R1))*p1';
 %W2 = (inv(R2))*p2';
 %W3 = (inv(R3))*p3';
@@ -58,7 +56,25 @@ end
 %W5 = (inv(R5))*p5';
 
 
-a3 = 0;
-b3 = [0];  
- 
+b = 1;
+  
+Zhat=[] ;
+for i = 1:5
+    b = [0; w{i}];
+    Zhat{end+1} = filter(b,1,Z);   
+end
 
+%er = {0,0,0,0,0};
+er= {};
+for L =1:5
+    er{end + 1} = Z - Zhat{L};
+end
+
+scaledZ2 = sqrt(19/219).*Z;
+%sound(scaledZ2, Fs);
+
+scaleder= {};
+for L =1:5
+    scaleder{end + 1} = sqrt(19/219)*er{L};
+end
+sound(scaleder{5}, Fs);
