dae98fad8d969466e3f70c7715bc24a97157e794
shuvipas <110031174+shuvipas@users.noreply.github.com>
2023-01-09 11:37:24 +0200

    Update LMS.m

--- LMS.m
+++ LMS.m
@@ -6,25 +6,32 @@ Zhat = zeros(length(Z), 1);
 reEr = zeros(length(Z), 1);
 estEr = zeros(length(Z), 1);
 normW = norm(w);
+
 for n = 1:length(Z)
+   
     for l = 1:L
-        if n-l < 1
-            break
-        else
-            Zhat(n) = Zhat(n) + w_lms(l) * Z(n-l);
-        end
-    end
-    estEr(n) = Zhat(n) - Z(n);
-    for l = 1:L
-        if n-L+(l-1) < 1
+        if n-l<1
             continue
-        else
-            w_lms(l) = w_lms(l) + mu*Z(n-L+(l-1))*estEr(n);
         end
+        Zhat(n) = Zhat(n)+ w_lms(l)*Z(n-l);
     end
-    reEr(n) = 10*log10((norm(w_lms - w)^2)/(normW^2));
-end
+    
+    estEr(n) = Zhat(n)  - Z(n);
+    c = w_lms -w;
+    reEr(n) = 10*log10(((norm(c))^2)/(normW^2));
+    
+    
+    for l =1:L
+        if n-l<1
+            continue
+        end
+        w_lms(l) =+ mu*estEr(n)*Z(n-l);
+    end
+    
+end    
+  
 NR = 10*log10(sum(Z.^2)/sum(estEr.^2));
 
 end
+
 
