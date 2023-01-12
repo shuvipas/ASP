c887f2c5a532322e4f6f17d9a0f7ff8736b86457
YosefSokilik <111966410+YosefSokilik@users.noreply.github.com>
2023-01-12 09:50:03 +0200

    Update adaptivepredict.m

--- adaptivepredict.m
+++ adaptivepredict.m
@@ -1,33 +1,29 @@
 function znext = adaptivepredict(zvec)
 %UNTITLED7 Summary of this function goes here
 %   Detailed explanation goes here
-L=5;
-delta=1e-9;
-lambda = 0.5;
-
-
+L = 4;
+delta = 1e-6;
+lambda = zeros(length(zvec), 1);
+lambda(1) = 0.95;
+for n = 2:length(lambda)
+    lambda(n) = 0.99*lambda(n-1)+(1-0.99);
+end
 wRls = zeros(L,1);
 P = (1/delta)*eye(L);
 error = zeros(length(zvec),1);
-estEr = zeros(length(zvec),1);
-
 
 for n = L:(length(zvec)-1)
     
-    U = zvec(n: -1:n-L+1);
-    dhat = wRls'*U;
-    
-    error(n+1) = zvec(n + 1) - dhat;
-    
-
-     k = ((1/lambda).*P*U)./(1+(1/lambda).*U'*P*U);
-     wRls =wRls + k.*error(n+1);
-     P = (1/lambda).*P-(1/lambda).*k*U'*P;
+     U = zvec(n: -1:n-L+1);
+     dhat = wRls'*U;
+     error(n+1) = zvec(n + 1) - dhat;
+     k = ((1/lambda(n)).*P*U)./(1+(1/lambda(n)).*U'*P*U);
+     wRls = wRls + k.*error(n+1);
+     P = (1/lambda(n)).*P-(1/lambda(n)).*k*U'*P;
     
 end
-NR = 10*log10(sum(zvec.^2)./sum(estEr.^2)); 
+
 Un = zvec(length(zvec): -1:length(zvec)-L+1);
 znext = wRls'*Un;
-
 
 end
