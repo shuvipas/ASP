8d28409188b18e0c49abb70d8b26c13ebb976296
shuvi pasko <110031174+shuvipas@users.noreply.github.com>
2023-03-10 11:52:16 +0200

    Update TX.m

--- Digital Communication/TX.m
+++ Digital Communication/TX.m
@@ -1,6 +1,7 @@
 function [ChannelInVec] = TX(config,infobits)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
+infobits = infobits(:)';
 BPSKbits = 1-2*infobits; %making bernuli vector
 pulseTrain = upsample(BPSKbits, config.Ts);%making the pulse train
 
@@ -12,6 +13,11 @@ else
 end
 
 ChannelInVec = conv(txpulse, pulseTrain);
+if(config.mod)
+    n = (1:length(ChannelInVec));
+    modulation = cos((2*pi*config.Fc*n)/(config.Fs));
+    ChannelInVec = ChannelInVec.*modulation;
+end
 ChannelInVec = [zeros(1, config.Fs) ChannelInVec zeros(1, 4*config.Fs)];
 
 end
