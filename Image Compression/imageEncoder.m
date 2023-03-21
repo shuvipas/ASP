01c5a940bf68e64a9034907e100a7a2d615db0e6
shuvi pasko <110031174+shuvipas@users.noreply.github.com>
2023-03-21 14:57:05 +0200

    Update imageEncoder.m

--- Image Compression/imageEncoder.m
+++ Image Compression/imageEncoder.m
@@ -8,6 +8,8 @@ Image = imread(image);
 BWimage = double(rgb2gray(Image));
 [height, width] = size(BWimage);
 M = 8;
+lastNZ = string(zeros(height/8,width/8));
+blocksEntropy = string(zeros(height/8,width/8));
 
 %Scale from [0, 255] to [−0.5, 127/256] by subtracting 128 and then dividing by 256.
 BWimageScale = (BWimage - 128)./256;
@@ -48,23 +50,45 @@ Gol_bitstream = strings(1, (height/M)*(width/M));
 BlocksZigZag = zeros(64, height/M, width/M);
 for i = height/M
     for j = width/M
-        tmp = ImBlocks(:,:,i,j);
         tmp = DPCM(:,:,i,j);
         BlocksZigZag(:,i,j) = tmp(ZigZag.ZigZagOrd);
 
     end 
 end
+%{
+%Searching for the last non-zero element
+for i = 1:height/M
+   for j = 1:width/M
+        current_block = BlocksZigZag(:,i,j);
+        current_lastNZ = find(current_block,1,'last');
+        if isempty(current_lastNZ)
+            current_lastNZ = 1;
+        end
+        lastNZ(i+1, j+1) = dec2bin(current_lastNZ-1,6);
+    end 
+end 
 
+%Entropy
+for i = 1:height/M
+   for j = 1:width/M
+       current_lastNZ = bin2dec(last(i,j))+1;
+       current_block = BlocksZigZag(:,i,j);
+       blocksEntropy(i, j) = golomb_enc(current_block(1:current_lastNZ));
+   
+   end
+end
+
+%}
 %Searching for the last non-zero element & Entropy coding
 for i = 1:height/M
    for j = 1:width/M
-       current_block = reshape(BlocksZigZag(:,i,j), [1, M*M]);
+       current_block = BlocksZigZag(:,i,j);
        current_lastNZ = find(current_block,1,'last');
        if isempty(current_lastNZ)
           current_lastNZ = 1;
        end
        index = sub2ind([height/M, width/M], i, j);
-       bitsoflastNZ = dec2bin(current_lastNZ - 1, 6);
+       bitsoflastNZ = dec2bin(current_lastNZ - 1, 6) -48;
        lastNZ_bit_stream(1 + 6*(index-1):6*index) = bitsoflastNZ;
        current_Gol_bitstream = golomb_enc(current_block(1:current_lastNZ));
        Gol_bitstream(index) = current_Gol_bitstream;
